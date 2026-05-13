48e7d51a54f89147c35691da42b94b7b4612774b
prashnam1234 <prashnamstha591@gmail.com>
2026-05-13 20:33:00 +0545

    Solving pattern problems using for loops

---
+++ Patterns/EasyAndMedium.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 1
+    // for (int i = 0; i < 5; i++) {
+    //     cout << "*****" << "\n";
+    // }
+
+    // 2
+    // for (int i = 0; i < 5; i++) {
+    //     for (int j = 0; j <= i; j++) {
+    //         cout << "*";
+    //     }
+    //     cout << "\n";
+    // }
+
+    // 3
+    // for (int i = 1; i <= 5; i++) {
+    //     for (int j = 1; j <= i; j++) {
+    //         cout << j;
+    //     }
+    //     cout << "\n";
+
+    // }
+
+    // 4
+    // for (int i = 1; i <= 5; i++) {
+    //     for (int j = 1; j <= i; j++) {
+    //         cout << i;
+    //     }
+    //     cout << "\n";
+    // }
+    
+    // 5
+    // for (int i = 5; i > 0; i--) {
+    //     for (int j = 0; j < i; j++) {
+    //         cout << "*";
+    //     }
+    //     cout << "\n";
+    // }
+
+    // 6
+    // for (int i = 5; i > 0; i--) {
+    //     for (int j = 1; j <= i; j++) {
+    //         cout << j;
+    //     }
+
+    //     cout << "\n";
+    // }
+
+
+    // 7
+    // 1 3 5 7 9
+    // for (int i = 0; i < 5; i++) {
+
+    //     for (int j = 4; j > i; j--) {
+    //         cout << " ";
+    //     }
+
+    //     for (int k = 0; k < (2*i+1); k++) {
+    //         cout << "*";
+    //     }
+    //     cout << "\n";
+    // }
+
+    // 8
+    // for (int i = 4; i >= 0; i--) {
+
+    //     for (int k = i; k < 4; k++) {
+    //         cout << " ";
+    //     }
+    //     for (int j = 0; j < (2*i+1); j++) {
+    //         cout << "*";
+    //     }
+    //     cout << "\n";
+    // }
+
+    // 9
+    // for (int i = 0; i < 5; i++) {
+    //     // space
+    //     for (int j = i; j < 4; j++ ) {
+    //         cout << " ";
+    //     }
+    //     // stars
+    //     for (int k = 0; k < (2*i+1); k++) {
+    //         cout << "*";
+    //     }
+    //     cout << "\n";
+    // }
+
+    // for (int i = 0; i < 5; i++) {
+    //     // space 
+    //     for (int j = 0; j < i; j++) {
+    //         cout << " ";
+    //     }
+
+    //     for (int k = (2*i+1); k <= 9; k++) {
+    //         cout << "*";
+    //     }
+    //     cout << "\n";
+    // }
+
+    // 10
+    for (int i = 0; i < 9; i++) {
+
+        for (int )
+    }
+
+    return 0;
+
+}
